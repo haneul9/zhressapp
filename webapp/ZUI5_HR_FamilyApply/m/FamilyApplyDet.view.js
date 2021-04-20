4c101832d8df2495cc4582c9b30f6a65edf64811
lotte-ktw <HPJT1006@lottechem.com>
2021-04-20 10:45:27 +0900

    가족사항 모바일 디자인 수정

--- webapp/ZUI5_HR_FamilyApply/m/FamilyApplyDet.view.js
+++ webapp/ZUI5_HR_FamilyApply/m/FamilyApplyDet.view.js
@@ -61,6 +61,7 @@ sap.ui.define([
 				new sap.m.Button(oController.PAGEID+"_Re",{text:oBundleText.getText("LABEL_44035"),press:oController.onRe,visible:{path:"Status",formatter:function(fVal){
 					return fVal=="88"?true:false;
 				}}}).addStyleClass("button-dark"),
+				new sap.ui.core.HTML({content:"<span>&nbsp;</span>"}),
 				new sap.m.Button({text:oBundleText.getText("LABEL_44031"),press:oController.onSave,visible:{path:"Opener",formatter:function(fVal){
 					return fVal=="X"?true:false;
 				}}}).addStyleClass("button-dark")]
