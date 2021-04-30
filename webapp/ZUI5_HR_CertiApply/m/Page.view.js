18ac345b4ee1863fe1d5d50bad7d9b7970baac08
yeonjun <hpjt0832@lottechem.com>
2021-04-30 10:56:14 +0900

    Squashed commit of the following:
    commit 43bdeb483d87781cd6f6a7a1fd9490e2fac57a42
    Author: yeonjun <hpjt0832@lottechem.com>
    Date:   Fri Apr 30 10:49:11 2021 +0900
    
        .
    
    commit 77ccc20f755bfd0387d577806ec29bcd02689cc7
    Author: yeonjun <hpjt0832@lottechem.com>
    Date:   Thu Apr 29 17:24:42 2021 +0900
    
        .
    
    commit 449519939cc48f63ead4a5d3f0480852e546f688
    Author: yeonjun <hpjt0832@lottechem.com>
    Date:   Thu Apr 29 17:14:45 2021 +0900
    
        .
    
    commit 871a82cbfc4ef3920702521f240956fa70149efe
    Author: yeonjun <hpjt0832@lottechem.com>
    Date:   Thu Apr 29 17:11:41 2021 +0900
    
        .
    
    commit 9c5143916fc4668e4c7f5b0176de4ad3007547e1
    Author: yeonjun <hpjt0832@lottechem.com>
    Date:   Thu Apr 29 17:09:05 2021 +0900
    
        .
    
    commit f89d38e00f47755879b4aa7a3f1bc983aafba6d7
    Author: yeonjun <hpjt0832@lottechem.com>
    Date:   Thu Apr 29 16:42:30 2021 +0900
    
        .
    
    commit 52fcd5fc3e3214341eaa337df78013f4a6dc23bd
    Author: yeonjun <hpjt0832@lottechem.com>
    Date:   Thu Apr 29 16:39:04 2021 +0900
    
        .
    
    commit ee2a58435d95fd7b8114adca37a688d635276d06
    Author: yeonjun <hpjt0832@lottechem.com>
    Date:   Thu Apr 29 16:04:18 2021 +0900
    
        .
    
    commit 2405b5c8d887da6da02ab9c99a1008396937df0c
    Author: yeonjun <hpjt0832@lottechem.com>
    Date:   Thu Apr 29 15:53:08 2021 +0900
    
        .

--- webapp/ZUI5_HR_CertiApply/m/Page.view.js
+++ webapp/ZUI5_HR_CertiApply/m/Page.view.js
@@ -16,121 +16,128 @@ sap.ui.define(
                 $.app.setModel("ZHR_CERTI_SRV");
                 $.app.setModel("ZHR_COMMON_SRV");
 
-                var oInfoBox = new sap.m.HBox({
-                    justifyContent: sap.m.FlexJustifyContent.SpaceBetween,
-                    fitContainer: true,
-                    items: [
-                        // 신청현황
-                        new sap.m.Label({ text: "{i18n>LABEL_38002}" }).addStyleClass("sub-title"),
-                        new sap.m.Button({
-                            press: function(){
-                            	oController.onPressReq();
-                            },
-                            text: "{i18n>LABEL_38044}" // 신청
-                        }).addStyleClass("button-light")
-                    ]
-                })
-                    .addStyleClass("info-box")
-                    .setModel(oController.LogModel);
-
-                var oTable = new sap.m.Table({
-                    inset: false,
-                    rememberSelections: false,
-                    noDataText: "{i18n>LABEL_00901}",
-                    growing: true,
-                    growingThreshold: 5,
-                    mode: sap.m.ListMode.SingleSelectMaster,
-                    columns: [
-                        new sap.m.Column({
-                            width: "40%",
-                            hAlign: "Begin"
-                        }),
-                        new sap.m.Column({
-                            width: "35%",
-                            hAlign: "Begin"
-                        }),
-                        new sap.m.Column({
-                            width: "25%",
-                            hAlign: "End"
-                        })
-                    ],
-                    items: {
-                        path: "/Data",
-                        template: new sap.m.ColumnListItem({
-                            type: sap.m.ListType.Active,
-                            counter: 5,
-                            cells: [
-                                new sap.m.VBox({
-                                    items: [
-                                        new sap.m.Text({
-                                            // 신청일
-                                            textAlign: "Begin",
-                                            text: {
-                                                path: "Begda",
-                                                formatter: function (v) {
-                                                    return v ? Common.DateFormatter(v) : "";
-                                                }
-                                            }
-                                        }).addStyleClass("L2P13Font font-14px"),
-                                        new sap.m.Text({
-                                            // 구분
-                                            text: "{Typetxt}",
-                                            textAlign: "Begin"
-                                        }).addStyleClass("L2P13Font font-14px")
-                                    ]
-                                }),
-                                new sap.m.VBox({
-                                    items: [
-                                        new sap.m.Text({
-                                            text: "{Zstatustxt}",
-                                            textAlign: "Begin"
-                                        }).addStyleClass("L2P13Font font-14px"),
-                                        new sap.m.Text({
-                                            // 구분
-                                            text: "{AptypT}",
-                                            textAlign: "Begin"
-                                        }).addStyleClass("L2P13Font font-14px")
-                                    ]
-                                }),
-                                new sap.m.VBox({
-                                    items: [
-                                        new sap.m.Button({
-                                            press: oController.onPressButton,
-                                            text: {
-                                                parts: [{ path: "Zstatus" }, { path: "Aptyp" }],
-                                                formatter: function (v, v2) {
-                                                    if (v === "1") {
-                                                        return oController.getBundleText("LABEL_65022"); // 처리중
-                                                    } else if (v === "2" && v2 === "1") {
-                                                        return oController.getBundleText("LABEL_65023"); // 재발급
-                                                    } else if (v === "3") {
-                                                        return oController.getBundleText("LABEL_65026"); // 다운로드
-                                                    }
-                                                }
-                                            },
-                                            visible: {
-                                                parts: [{ path: "Zstatus" }, { path: "Aptyp" }],
-                                                formatter: function (v, v2) {
-                                                    if (v === "2") {
-                                                        if (v2 === "1") return true;
-                                                        else return false;
-                                                    } else {
-                                                        return true;
-                                                    }
-                                                }
-                                            }
-                                        }).addStyleClass("button-light-sm")
-                                    ]
-                                })
-                            ]
-                        })
-                    }
-                }).setModel(oController.TableModel);
-
                 return new PageHelper({
                     contentContainerStyleClass: "app-content-container-mobile",
-                    contentItems: [oInfoBox, oTable]
+                    contentItems: [this.MainingBox(oController)]
                 });
+            },
+
+            MainingBox: function(oController) {
+
+
+                return new sap.m.VBox({
+                    items: [
+                        new sap.m.HBox({
+                            justifyContent: sap.m.FlexJustifyContent.SpaceBetween,
+                            fitContainer: true,
+                            items: [
+                                // 신청현황
+                                new sap.m.Label({ text: "{i18n>LABEL_38002}" }).addStyleClass("sub-title"),
+                                new sap.m.Button({
+                                    press: function(){
+                                        oController.onPressReq();
+                                    },
+                                    text: "{i18n>LABEL_38044}" // 신청
+                                }).addStyleClass("button-light")
+                            ]
+                        }).addStyleClass("info-box")
+                          .setModel(oController.LogModel),
+        
+                        new sap.m.Table({
+                            inset: false,
+                            rememberSelections: false,
+                            noDataText: "{i18n>LABEL_00901}",
+                            growing: true,
+                            growingThreshold: 5,
+                            mode: sap.m.ListMode.SingleSelectMaster,
+                            columns: [
+                                new sap.m.Column({
+                                    width: "40%",
+                                    hAlign: "Begin"
+                                }),
+                                new sap.m.Column({
+                                    width: "35%",
+                                    hAlign: "Begin"
+                                }),
+                                new sap.m.Column({
+                                    width: "25%",
+                                    hAlign: "End"
+                                })
+                            ],
+                            items: {
+                                path: "/Data",
+                                template: new sap.m.ColumnListItem({
+                                    type: sap.m.ListType.Active,
+                                    counter: 5,
+                                    cells: [
+                                        new sap.m.VBox({
+                                            items: [
+                                                new sap.m.Text({
+                                                    // 신청일
+                                                    textAlign: "Begin",
+                                                    text: {
+                                                        path: "Begda",
+                                                        formatter: function (v) {
+                                                            return v ? Common.DateFormatter(v) : "";
+                                                        }
+                                                    }
+                                                }).addStyleClass("L2P13Font font-14px"),
+                                                new sap.m.Text({
+                                                    // 구분
+                                                    text: "{Typetxt}",
+                                                    textAlign: "Begin"
+                                                }).addStyleClass("L2P13Font font-14px")
+                                            ]
+                                        }),
+                                        new sap.m.VBox({
+                                            items: [
+                                                new sap.m.Text({
+                                                    text: "{Zstatustxt}",
+                                                    textAlign: "Begin"
+                                                }).addStyleClass("L2P13Font font-14px"),
+                                                new sap.m.Text({
+                                                    // 구분
+                                                    text: "{AptypT}",
+                                                    textAlign: "Begin"
+                                                }).addStyleClass("L2P13Font font-14px")
+                                            ]
+                                        }),
+                                        new sap.m.VBox({
+                                            items: [
+                                                new sap.m.Button({
+                                                    press: oController.onPressButton,
+                                                    text: {
+                                                        parts: [{ path: "Zstatus" }, { path: "Aptyp" }],
+                                                        formatter: function (v, v2) {
+                                                            if (v === "1") {
+                                                                return oController.getBundleText("LABEL_65022"); // 처리중
+                                                            } else if (v === "2" && v2 === "1") {
+                                                                return oController.getBundleText("LABEL_65023"); // 재발급
+                                                            } else if (v === "3") {
+                                                                return oController.getBundleText("LABEL_65026"); // 다운로드
+                                                            }
+                                                        }
+                                                    },
+                                                    visible: {
+                                                        parts: [{ path: "Zstatus" }, { path: "Aptyp" }],
+                                                        formatter: function (v, v2) {
+                                                            if (v === "2") {
+                                                                if (v2 === "1") return true;
+                                                                else return false;
+                                                            } else {
+                                                                return true;
+                                                            }
+                                                        }
+                                                    }
+                                                }).addStyleClass("button-light-sm")
+                                            ]
+                                        })
+                                    ]
+                                })
+                            }
+                        }).setModel(oController.TableModel)
+                    ]
+                }).addStyleClass("vbox-form-mobile");
             }
         });
     }
