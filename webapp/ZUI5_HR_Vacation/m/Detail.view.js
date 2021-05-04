57e3ae7a4f154c03346f3f9840553f3709abfac1
Jiyoung Pyo <HPJT0847@lottechem.com>
2021-05-04 09:32:00 +0000

    근태신청 모바일 width 수정

--- webapp/ZUI5_HR_Vacation/m/Detail.view.js
+++ webapp/ZUI5_HR_Vacation/m/Detail.view.js
@@ -16,7 +16,7 @@ sap.ui.jsview("ZUI5_HR_Vacation.m.Detail", {
 		var oMatrix = new sap.ui.commons.layout.MatrixLayout({
 			columns : 2,
 			width : "100%",
-			widths : ["30%", "70%"],
+			widths : ["105px", ""],
 			rows : [new sap.ui.commons.layout.MatrixLayoutRow({
 						height : "45px",
 						cells : [new sap.ui.commons.layout.MatrixLayoutCell({
