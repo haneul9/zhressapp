6f3a9fb7fbcb275358bea50f05cc984075f69632
lotte-ktw <HPJT1006@lottechem.com>
2021-05-06 20:18:22 +0900

    matrixlayout columns 수정

--- webapp/ZUI5_HR_MedApply/m/MedApplyDet.view.js
+++ webapp/ZUI5_HR_MedApply/m/MedApplyDet.view.js
@@ -460,7 +460,7 @@ sap.ui.define([
 
 			oRow=new sap.ui.commons.layout.MatrixLayoutRow();
 			oCell=new sap.ui.commons.layout.MatrixLayoutCell({ 
-				colSpan:9,
+				colSpan:2,
 				content:new sap.ui.core.HTML({content:"<div style='height:3px;'></div><span style='color:red;font-size:14px;'>"+oController.getBundleText("MSG_47038")+"</span>"})
 			}).addStyleClass("DataCell");
 			oRow.addCell(oCell);
