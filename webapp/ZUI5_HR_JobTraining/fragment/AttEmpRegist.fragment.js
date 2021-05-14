1e5d28e7918cc981d24ad0abb2a68f3cd5329df3
JHU <HPJT0897@lottechem.com>
2021-05-14 16:08:13 +0900

    Merge 0514 Create JobTraining

---
+++ webapp/ZUI5_HR_JobTraining/fragment/AttEmpRegist.fragment.js
@@ -0,0 +1,108 @@
+﻿sap.ui.define([
+    "../../common/ZHR_TABLES"
+], function (ZHR_TABLES) {
+"use strict";
+
+    sap.ui.jsfragment("ZUI5_HR_JobTraining.fragment.AttEmpRegist", {
+
+        _colModel: [
+			{id: "Orgtx1", 	    label: "{i18n>LABEL_70055}"/* 부서 */,plabel: "", resize: true, span: 0, type: "template",  sort: true,  filter: true, width: "25%", templateGetter:"getDeft"},
+			{id: "ZpGradeTxt", 	label: "{i18n>LABEL_70043}"/* 직급 */,	plabel: "", resize: true, span: 0, type: "string",  sort: true,  filter: true, width: "25%"},
+			{id: "Pernr", 		label: "{i18n>LABEL_70044}"/* 사번 */,	plabel: "", resize: true, span: 0, type: "string",  sort: true,  filter: true, width: "25%"},
+			{id: "Ename",   	label: "{i18n>LABEL_70045}"/* 성명 */,  plabel: "", resize: true, span: 0, type: "string", 	sort: true,  filter: true, width: "25%"}
+		],
+
+		createContent: function (oController) {
+
+            var oAttTypeCombo = new sap.m.ComboBox({ // 교육효과평가
+				width: "100px",
+				items: {
+					path: "/AttTypeCombo",
+					template: new sap.ui.core.ListItem({
+						key: "{Code}",
+						text: "{Text}"
+					})
+				},
+				selectedKey: "{AttType}"
+			});
+			
+			// 키보드 입력 방지
+			oAttTypeCombo.addDelegate({
+				onAfterRendering: function () {
+					oAttTypeCombo.$().find("INPUT").attr("disabled", true).css("color", "#ccc !important");
+				}
+            }, oAttTypeCombo);
+
+            var oAttSerchBox = new sap.m.FlexBox({
+				justifyContent: sap.m.FlexJustifyContent.SpaceBetween,
+				fitContainer: true,
+				items: [
+					new sap.m.HBox({
+						items: [
+							new sap.m.Label({ text: "{i18n>LABEL_70056}" }), // 검색
+                            oAttTypeCombo,
+                            new sap.m.Input({
+                                textAlign: "Begin",
+                                width: "450px",
+                                value: "{EmpValue}"
+                            })
+						]
+					}).addStyleClass("search-field-group"),
+					new sap.m.HBox({
+						items: [							
+							new sap.m.Button({
+								press: oController.onAttSearch.bind(oController),
+								text: "{i18n>LABEL_70007}" // 조회
+							}).addStyleClass("button-search")
+						]
+					})
+					.addStyleClass("button-group")
+				]
+            })
+            .setModel(oController.AttSearchModel)
+			.bindElement("/SearchData")
+			.addStyleClass("search-box search-bg pb-7px mt-16px");
+
+            var oAttTable = new sap.ui.table.Table(oController.PAGEID + "_AttTable2", {
+				selectionMode: sap.ui.table.SelectionMode.None,
+				enableColumnReordering: false,
+				enableColumnFreeze: false,
+				enableBusyIndicator: true,
+				visibleRowCount: 10,
+				showOverlay: false,
+				showNoData: true,
+				width: "auto",
+				rowHeight: 37,
+				columnHeaderHeight: 38,
+				noData: "{i18n>LABEL_00901}"
+			})
+			.addStyleClass("mt-30px")
+			.setModel(oController.AttDetailModel)
+			.attachCellClick(oController.onSelectedAttRow)
+			.bindRows("/Data");
+			
+			ZHR_TABLES.makeColumn(oController, oAttTable, this._colModel); 
+
+			var oDialog = new sap.m.Dialog({
+				title: "{i18n>LABEL_70057}",    // 사원 검색
+				contentWidth: "850px",
+				contentHeight: "560px",
+				buttons: [
+					new sap.m.Button({
+						press: function () {
+							oDialog.close();
+						},
+						text: "{i18n>LABEL_29028}" // 닫기
+					}).addStyleClass("button-default")
+				],
+				content: [
+                    oAttSerchBox,
+                    oAttTable
+                ]
+            })
+			.addStyleClass("custom-dialog-popup");
+
+			return oDialog;
+		}
+	});
+});
---
+++ webapp/ZUI5_HR_JobTraining/fragment/TeacherRegist.fragment.js
@@ -0,0 +1,86 @@
+﻿sap.ui.define([
+    "../../common/ZHR_TABLES"
+], function (ZHR_TABLES) {
+"use strict";
+
+    sap.ui.jsfragment("ZUI5_HR_JobTraining.fragment.TeacherRegist", {
+
+        _colModel: [
+			{id: "Stext1", 	    label: "{i18n>LABEL_70042}"/* 부서명 */,plabel: "", resize: true, span: 2, type: "string",  sort: true,  filter: true, width: "auto"},
+			{id: "Stext2", 	    label: "{i18n>LABEL_70042}"/* 부서명 */,plabel: "", resize: true, span: 0, type: "string",  sort: true,  filter: true, width: "auto"},
+			{id: "PGradeTxt", 	label: "{i18n>LABEL_70043}"/* 직급 */,	plabel: "", resize: true, span: 0, type: "string",  sort: true,  filter: true, width: "20%"},
+			{id: "Pernr", 		label: "{i18n>LABEL_70044}"/* 사번 */,	plabel: "", resize: true, span: 0, type: "string",  sort: true,  filter: true, width: "20%"},
+			{id: "Ename",   	label: "{i18n>LABEL_70045}"/* 성명 */,  plabel: "", resize: true, span: 0, type: "string", 	sort: true,  filter: true, width: "20%"}
+		],
+
+		createContent: function (oController) {
+
+            var oTraningBox = new sap.m.FlexBox({
+				justifyContent: sap.m.FlexJustifyContent.SpaceBetween,
+				fitContainer: true,
+				items: [
+					new sap.m.HBox({
+						items: [
+							new sap.m.Label({ text: "{i18n>LABEL_70045}" }), // 성명
+                            new sap.m.Input(oController.PAGEID + "DTeacherInput", {
+                                textAlign: "Begin",
+                                width: "450px"
+                            })
+						]
+					}).addStyleClass("search-field-group"),
+					new sap.m.HBox({
+						items: [							
+							new sap.m.Button({
+								press: oController.onTeacherSearch.bind(oController),
+								text: "{i18n>LABEL_70007}" // 조회
+							}).addStyleClass("button-search")
+						]
+					})
+					.addStyleClass("button-group")
+				]
+            })
+			.addStyleClass("search-box search-bg pb-7px mt-16px");
+
+            var oTeacherTable2 = new sap.ui.table.Table(oController.PAGEID + "_TeacherTable", {
+				selectionMode: sap.ui.table.SelectionMode.None,
+				enableColumnReordering: false,
+				enableColumnFreeze: false,
+				enableBusyIndicator: true,
+				visibleRowCount: 10,
+				showOverlay: false,
+				showNoData: true,
+				width: "auto",
+				rowHeight: 37,
+				columnHeaderHeight: 38,
+				noData: "{i18n>LABEL_00901}"
+			})
+			.addStyleClass("mt-30px")
+			.setModel(oController.TeacherModel)
+			.attachCellClick(oController.onSelectedTeacherRow)
+			.bindRows("/Data");
+			
+			ZHR_TABLES.makeColumn(oController, oTeacherTable2, this._colModel); 
+
+			var oDialog = new sap.m.Dialog({
+				title: "{i18n>LABEL_70046}",    // 강사 검색
+				contentWidth: "850px",
+				contentHeight: "560px",
+				buttons: [
+					new sap.m.Button({
+						press: function () {
+							oDialog.close();
+						},
+						text: "{i18n>LABEL_29028}" // 닫기
+					}).addStyleClass("button-default")
+				],
+				content: [
+                    oTraningBox,
+                    oTeacherTable2
+                ]
+            })
+			.addStyleClass("custom-dialog-popup");
+
+			return oDialog;
+		}
+	});
+});
