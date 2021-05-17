ebe1a4beb602c784cf2be9779f111c56912bdd9d
JHU <HPJT0897@LOTTECHEM.COM>
2021-05-17 20:07:23 +0900

    x

--- webapp/ZUI5_HR_Home/js/portlets/EvalGoalPortlet.js
+++ webapp/ZUI5_HR_Home/js/portlets/EvalGoalPortlet.js
@@ -95,12 +95,14 @@ fill: function() {
 						}
 
 						oList3.push({iDone: iDone, index: i, vGroundColor: oGroundColor});
+						
+						var vName = (v.name || '').split(/\n/)[0] + "\n" + (v.name || '').split(/\n/)[1];
 
 						list.append([
 							'<div class="my-evalgoal-info">',
 								'<div class="mylist">',
 									// i + 1 + '.' + v.name,
-									(v.name || '').split(/\n/)[0] + "\n" + (v.name || '').split(/\n/)[1],
+									vName,
 								'</div>',
 								'<div class="evalgoal-statusBar" title="' + iDone + '%">',
 									'<div class="progress">',
