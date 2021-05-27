8cc8464a5c26826f782781208d077667142a9e90
JHU <HPJT0897@LOTTECHEM.COM>
2021-05-27 21:19:38 +0900

    x

--- webapp/ZUI5_HR_Suggestions/m/Regist.controller.js
+++ webapp/ZUI5_HR_Suggestions/m/Regist.controller.js
@@ -77,41 +77,41 @@
 		onBeforeShow: function(oEvent) {
 			BusyIndicator.show(0);
 
-            // this.RegistModel.setData({FormData: []});
-            // var oDateBox = $.app.byId(this.PAGEID + "_RegistDateBox");
-            // var oIsHideBox = $.app.byId(this.PAGEID + "_IsHideBox");
-            // var oCommentBox = $.app.byId(this.PAGEID + "_CommentBox");
+            this.RegistModel.setData({FormData: []});
+            var oDateBox = $.app.byId(this.PAGEID + "_RegistDateBox");
+            var oIsHideBox = $.app.byId(this.PAGEID + "_IsHideBox");
+            var oCommentBox = $.app.byId(this.PAGEID + "_CommentBox");
 
-            // if(oEvent.data){
-            //     this.RegistModel.setData({ FormData: oEvent.data.RowData ? oEvent.data.RowData : [] });
-            //     this.CommentModel.setData({Data: {}});
-            //     if(Common.checkNull(oEvent.data.RowData)){
-            //         this.RegistModel.setData({FormData: []});
-            //         oCommentBox.destroyItems();
-            //         oDateBox.setVisible(false);
-            //         oIsHideBox.setVisible(true);
-			// 		this.CommentModel.setProperty("/HideComment", "X");
-            //     }else{
-            //         oDateBox.setVisible(true);
-            //         oIsHideBox.setVisible(false);
-            //         this.getDetailData(this, "O");
-            //     }
-            // }
+            if(oEvent.data){
+                this.RegistModel.setData({ FormData: oEvent.data.RowData ? oEvent.data.RowData : [] });
+                this.CommentModel.setData({Data: {}});
+                if(Common.checkNull(oEvent.data.RowData)){
+                    this.RegistModel.setData({FormData: []});
+                    oCommentBox.destroyItems();
+                    oDateBox.setVisible(false);
+                    oIsHideBox.setVisible(true);
+					this.CommentModel.setProperty("/HideComment", "X");
+                }else{
+                    oDateBox.setVisible(true);
+                    oIsHideBox.setVisible(false);
+                    this.getDetailData(this, "O");
+                }
+            }
 
 			Common.log("onBeforeShow");
 		},
 		
 		onAfterShow: function() {
-            // this.onBeforeOpenDetailDialog();
+            this.onBeforeOpenDetailDialog();
 			BusyIndicator.hide();
         },
 
         navBack: function() {
             sap.ui.getCore().getEventBus().publish("nav", "to", {
                 id: [$.app.CONTEXT_PATH, "Page"].join($.app.getDeviceSuffix()),
-				data: { 
-                    New: "X"
-                }
+				// data: { 
+                //     New: "X"
+                // }
             });
         },
 
@@ -121,7 +121,7 @@
 			
 			var sendObject = {};
 
-			if(New === "O")	sendObject.IEss = "X";
+			// if(New === "O")	sendObject.IEss = "X";
 
 			// Header
 			sendObject.ISdate = oController.RegistModel.getProperty("/FormData/Sdate");
@@ -164,10 +164,10 @@
 			oCommentBox.destroyItems();
 
 			vCommData.forEach(function(e, i) {
-				var vGood = localStorage.getItem("ehr.suggestions." + oController.RegistModel.getProperty("/FormData/Sdate") + oController.RegistModel.getProperty("/FormData/Seqnr") + e.Seqnr2 + ".goodconfirmed") === "Y";
-				var vBed = localStorage.getItem("ehr.suggestions." + oController.RegistModel.getProperty("/FormData/Sdate") + oController.RegistModel.getProperty("/FormData/Seqnr") + e.Seqnr2 + ".bedconfirmed") === "N";
+				// var vGood = localStorage.getItem("ehr.suggestions." + oController.RegistModel.getProperty("/FormData/Sdate") + oController.RegistModel.getProperty("/FormData/Seqnr") + e.Seqnr2 + ".goodconfirmed") === "Y";
+				// var vBed = localStorage.getItem("ehr.suggestions." + oController.RegistModel.getProperty("/FormData/Sdate") + oController.RegistModel.getProperty("/FormData/Seqnr") + e.Seqnr2 + ".bedconfirmed") === "N";
 
-				if((Common.checkNull(e.Zdel) && oController.RegistModel.getProperty("/SubCommentData").length === 0) || oController.RegistModel.getProperty("/SubCommentData").some(function(ele) { return ele.Seqnr2 === e.Seqnr2 || (ele.Seqnr2 !== e.Seqnr2 && Common.checkNull(e.Zdel)); })) {
+				// if((Common.checkNull(e.Zdel) && oController.RegistModel.getProperty("/SubCommentData").length === 0) || oController.RegistModel.getProperty("/SubCommentData").some(function(ele) { return ele.Seqnr2 === e.Seqnr2 || (ele.Seqnr2 !== e.Seqnr2 && Common.checkNull(e.Zdel)); })) {
 					oCommentBox.addItem(
 						new sap.m.VBox({
 							fitContainer: true,
@@ -267,17 +267,17 @@
 										new sap.m.Button({ 
 											icon: "sap-icon://thumb-up",
 											text: "{i18n>LABEL_56020}", // 좋아요
-											enabled: (vGood === false && vBed === false) || vGood,
+											// enabled: (vGood === false && vBed === false) || vGood,
 											press: oController.OnCommThumbUp.bind(oController)
-										}).addStyleClass("button-light-sm")
-										.toggleStyleClass("button-HiTokTok-check", vGood),
+										}).addStyleClass("button-light-sm"),
+										// .toggleStyleClass("button-HiTokTok-check", vGood),
 										new sap.m.Button({ 
 											icon: "sap-icon://thumb-down",
 											text: "{i18n>LABEL_56021}", // 싫어요
-											enabled: (vGood === false && vBed === false) || vBed,
+											// enabled: (vGood === false && vBed === false) || vBed,
 											press: oController.OnCommThumbDown.bind(oController)
-										}).addStyleClass("button-light-sm")
-										.toggleStyleClass("button-HiTokTok-check", vBed),
+										}).addStyleClass("button-light-sm"),
+										// .toggleStyleClass("button-HiTokTok-check", vBed),
 										new sap.m.Button({
 											press: oController.onCommentSubBtn.bind(oController),
 											icon: "sap-icon://comment",
@@ -315,9 +315,9 @@
 						.addStyleClass("custom-HiTokTok-comment mt-5px")
 					);
 	
-					oController.setSubComments(e, vIndex);
-					vIndex = vIndex + 1;
-				}
+					oController.setSubComments(e, i);
+					// vIndex = vIndex + 1;
+				// }
 			});
 		},
 
@@ -329,8 +329,8 @@
 			if(Common.checkNull(!index) || index === 0){ // 저장된 대댓글 Setting
 				vSubCommentData.forEach(function(e) {
 					if(e.Seqnr2 === oEvent.Seqnr2) {
-						var vGood = localStorage.getItem("ehr.suggestions." + oController.RegistModel.getProperty("/FormData/Sdate") + oController.RegistModel.getProperty("/FormData/Seqnr") + oEvent.Seqnr2 + e.Seqnr3 + ".goodconfirmed") === "Y";
-						var vBed = localStorage.getItem("ehr.suggestions." + oController.RegistModel.getProperty("/FormData/Sdate") + oController.RegistModel.getProperty("/FormData/Seqnr") + oEvent.Seqnr2 + e.Seqnr3 + ".bedconfirmed") === "N";
+						// var vGood = localStorage.getItem("ehr.suggestions." + oController.RegistModel.getProperty("/FormData/Sdate") + oController.RegistModel.getProperty("/FormData/Seqnr") + oEvent.Seqnr2 + e.Seqnr3 + ".goodconfirmed") === "Y";
+						// var vBed = localStorage.getItem("ehr.suggestions." + oController.RegistModel.getProperty("/FormData/Sdate") + oController.RegistModel.getProperty("/FormData/Seqnr") + oEvent.Seqnr2 + e.Seqnr3 + ".bedconfirmed") === "N";
 
 						oCommentBox.getItems()[index].getItems()[3].setVisible(true);
 						oCommentBox.getItems()[index].getItems()[3].addItem(
@@ -431,17 +431,17 @@
 											new sap.m.Button({ // 좋아요
 												icon: "sap-icon://thumb-up",
 												text: "{i18n>LABEL_56020}",
-												enabled: (vGood === false && vBed === false) || vGood,
+												// enabled: (vGood === false && vBed === false) || vGood,
 												press: oController.OnReCommThumbUp.bind(oController)
-											}).addStyleClass("button-light-sm")
-											.toggleStyleClass("button-HiTokTok-check", vGood),
+											}).addStyleClass("button-light-sm"),
+											// .toggleStyleClass("button-HiTokTok-check", vGood),
 											new sap.m.Button({ // 싫어요
 												icon: "sap-icon://thumb-down",
 												text: "{i18n>LABEL_56021}",
-												enabled: (vGood === false && vBed === false) || vBed,
+												// enabled: (vGood === false && vBed === false) || vBed,
 												press: oController.OnReCommThumbDown.bind(oController)
-											}).addStyleClass("button-light-sm")
-											.toggleStyleClass("button-HiTokTok-check", vBed),
+											}).addStyleClass("button-light-sm"),
+											// .toggleStyleClass("button-HiTokTok-check", vBed),
 											new sap.m.Button({
 												press: oController.onSubCommentReBtn.bind(oController),
 												text: "{i18n>LABEL_56013}" // 수정
