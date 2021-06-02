f9194c37bc535875c49092abecd69fa68d171eaf
JHU <HPJT0897@LOTTECHEM.COM>
2021-06-02 17:10:36 +0900

    x

--- webapp/ZUI5_HR_MedApply/MedApply.controller.js
+++ webapp/ZUI5_HR_MedApply/MedApply.controller.js
@@ -945,7 +945,7 @@ sap.ui.define(
                     return ;
                 }
                 
-                if (New === "N" && oController._MedDateChange === "X" && oController._GubunBukrs !== Flag) {
+                if (New === "N" && (New === "N" && oController._MedDateChange === "O" && oController._GubunBukrs !== Flag)) {
                     oController.initTdata(Flag);
 
                     if(Flag === "A100") 
