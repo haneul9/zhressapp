9c82801c13811d79367ef3d153be76decb3bc958
JHU <HPJT0897@LOTTECHEM.COM>
2021-06-14 18:01:18 +0900

    Fix 톡톡 에딧 width

--- webapp/ZUI5_HR_Suggestions/Detail.controller.js
+++ webapp/ZUI5_HR_Suggestions/Detail.controller.js
@@ -1578,7 +1578,7 @@
 				that.oRichTextEditor = new RTE("myRTE", {
 					editorType: EditorType.TinyMCE4,
 					layoutData: new sap.m.FlexItemData({ growFactor: 1 }),
-					width: "90%",
+					width: "100%",
 					height: "500px",
 					customToolbar: true,
 					showGroupFont: true,
